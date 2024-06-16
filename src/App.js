c0aa5467ad7123046330db912eb4c5dd35013d55
Levladik <vladislavtyshenko@yandex.ru>
2024-06-16 21:19:03 +0400

    add layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React, { useState } from'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const [showGif, setShowGif] = useState(false);
+
+  const handleClick = () => {
+    setShowGif(!showGif);
+  };
+
   return (
     <div className="App">
-      TEST
+      <Container maxWidth="sm">
+        <div>
+          Типа офер, с кратким изложением сути проекта
+        </div>
+        <div>
+          <TwitterIcon/>
+          <TelegramIcon/>
+          <YouTubeIcon/>
+        </div>
+        <div>
+          Инструкция покупки
+        </div>
+        <Button variant="contained" onClick={handleClick}>бабло</Button>
+        {showGif && (
+          <img src="https://media1.tenor.com/m/ng0mwYEEpNMAAAAd/hasbulla-money.gif" alt="Hasbulla Money GIF" />
+        )}
+      </Container>      
     </div>
   );
 }
